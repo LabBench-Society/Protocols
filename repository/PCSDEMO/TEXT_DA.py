58ce44a8b9de7f6f3b38556748b47e5a05166a8b
Kristian Hennings <krhe@inventors.dk>
2023-01-22 19:32:47 +0100

    Added localization

---
+++ repository/PCSDEMO/TEXT_DA.py
@@ -0,0 +1,31 @@
+def CreateText():
+   	return {
+		"QUESTION": "When I'm in pain ... [DK]",
+		"I01": "I worry all the time about whether the pain will end. [DK]",
+		"I02": "I feel I can't go on. [DK]",
+		"I03": "It's terrible and I think it's never going to get any better [DK]",
+		"I04": "It's awful and I feel that it overwhelms me [DK]",
+		"I05": "I feel I can't stand it anymore [DK]",
+		"I06": "I become afraid that the pain will get worse [DK]",
+		"I07": "I keep thinking of other painful events [DK]",
+		"I08": "I anxiously want the pain to go away [DK]",
+		"I09": "I can't seem to keep it out of my mind [DK]",
+		"I10": "I keep thinking about how much it hurts [DK]",
+		"I11": "I keep thinking about how badly I want the pain to stop [DK]",
+		"I12": "There's nothing I can do to reduce the intensity of the pain [DK]",
+		"I13": "I wonder whether something serious may happen [DK]",
+		"L0": "0) Not at all [DK]",
+		"L1": "1) To a slight degree [DK]",
+		"L2": "2) To a moderate degree [DK]",
+		"L3": "3) To a great degree [DK]",
+		"L4": "4) All the time [DK]"
+	}
+
+
+
+
+
+
+
+
+
