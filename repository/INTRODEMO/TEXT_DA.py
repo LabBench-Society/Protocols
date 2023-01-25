2e1eddef753a91c5c3862cb5fbe16a718a1da7ce
Kristian Hennings <krhe@inventors.dk>
2023-01-25 20:22:09 +0100

    Work on a non-copyrighted Introduction protocol

--- repository/INTRODEMO/TEXT_DA.py
+++ repository/INTRODEMO/TEXT_DA.py
@@ -1,24 +1,10 @@
 def CreateText():
    	return {
-		"QUESTION": "Når jeg har smerte ...",
-		"I01": "Det bekymrer mig hele tiden, om smerterne vil forsvinde",
-		"I02": "Jeg føler, at jeg ikke kan mere",
-		"I03": "Det er frygteligt, og jeg tænker, at det aldrig bliver bedre",
-		"I04": "Det er forfærdeligt, og jeg føler mig overvældet af smerterne",
-		"I05": "Jeg føler, at jeg ikke kan holde det ud længere",
-		"I06": "Jeg bliver bange for at smerterne bliver værre",
-		"I07": "Jeg tænker hele tiden på andre smertefulde oplevelser",
-		"I08": "Jeg ønsker desperat, at smerterne vil forsvinde",
-		"I09": "Jeg kan ikke lade være med at tænke på mine smerter",
-		"I10": "Jeg bliver ved med at tænke på hvor ondt det gør",
-		"I11": "Jeg bliver ved med at tænke på, hvor meget jeg ønsker, at smerterne skal holde op",
-		"I12": "Der er intet, jeg kan gøre for at mindske intensiteten af mine smerterne",
-		"I13": "Jeg tænker på, om der kunne ske noget alvorligt",
-		"L0": "Slet ikke",
-		"L1": "I ringe grad",
-		"L2": "I nogen grad",
-		"L3": "I høj grad",
-		"L4": "I meget høj grad"
+		"I01": "Jeg blev bragt ud af ligevægt af ret ligegyldige ting",
+		"L0": " Passede ikke på mig",
+		"L1": "Passede en smule eller noget af tiden på mig",
+		"L2": "Passede I betydelig grad eller en stor del af tiden på mig",
+		"L3": "Passede rigtigt meget eller det meste af tiden på mig"
 	}
 
 
