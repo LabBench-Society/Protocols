e412c03865d45038bf9498bf2ff9b63a00977008
Kristian Hennings <krhe@inventors.dk>
2023-09-24 16:04:07 +0200

    Created the neutral task in the new format

--- docs/stroop/Script.py
+++ docs/stroop/Script.py
@@ -2,97 +2,60 @@
 import traceback
 import random
 
-class StroopTask:
-    def __init__(self, tc):
-        if tc.Language == "DA":
-            stimuli = tc.Assets.DanishImages
-        else:
-            stimuli = tc.Assets.EnglishImages
-            
-        self.blank = stimuli.GetAsset("blank.PNG")
-        
-        self.congruent = {
-                "r": stimuli.GetAsset("rr.PNG"),
-                "g": stimuli.GetAsset("gg.PNG"),
-                "b": stimuli.GetAsset("bb.PNG"),
-                "y": stimuli.GetAsset("yy.PNG")
-            }
-        self.neutral = {
-                "r": stimuli.GetAsset("rn.PNG"),
-                "g": stimuli.GetAsset("gn.PNG"),
-                "b": stimuli.GetAsset("bn.PNG"),
-                "y": stimuli.GetAsset("yn.PNG")
-            }
-        self.incongruent = {
-                "rg": stimuli.GetAsset("rg.PNG"),
-                "rb": stimuli.GetAsset("rb.PNG"),
-                "ry": stimuli.GetAsset("ry.PNG"),
-                "gr": stimuli.GetAsset("gr.PNG"),
-                "gb": stimuli.GetAsset("gb.PNG"),
-                "gy": stimuli.GetAsset("gy.PNG"),
-                "br": stimuli.GetAsset("br.PNG"),
-                "by": stimuli.GetAsset("by.PNG"),
-                "bg": stimuli.GetAsset("bg.PNG"),
-                "yr": stimuli.GetAsset("yr.PNG"),
-                "yb": stimuli.GetAsset("yb.PNG"),
-                "yg": stimuli.GetAsset("yg.PNG"),
-            }
-        
-    def initialize(self, tc):
-        self.numberOfStimuli = tc.NumberOfCongruentStimuli * tc.CongruentRepetitions
-        
-        ckeys = list(self.congruent.keys())
-        self.congruentId = [ckeys[n % len(self.congruent)] for n in range(self.numberOfStimuli)]
-        self.congruentIndex = 0
-    
-    def getCongruent(self):
-        if self.congruentIndex >= self.numberOfStimuli:
-            raise ValueError("Congruent index out of bounds")
-        
-        retValue = self.congruent[self.congruentId[self.congruentIndex]].Data
-        self.congruentIndex = self.congruentIndex + 1                          
-        return retValue
-    
-    def getNeutral(self):
-        return self.neutral["g"].Data
+def generateSequence(size, images):
+    keys = list(images.keys())
+    retValue = [keys[n % len(keys)] for n in range(size)]
+    random.shuffle(retValue)
+    return retValue
 
-    def getIncongruent(self):
-        return self.incongruent["rg"].Data 
+def getImages(tc):
+    if tc.Language == "DA":
+        return tc.Assets.DanishImages
+    else:
+        return tc.Assets.EnglishImages
     
-    def getBlank(self):
-        return self.blank.Data
+def getBlank(tc):
+    images = getImages(tc)
+    return images.GetAsset("blank.PNG").Data
 
-def CreateTask(tc):
-    return StroopTask(tc)
-
+def LoadImages(tc):
+    images = getImages(tc)
+    return {
+                "rr": images.GetAsset("rr.PNG").Data,
+                "gg": images.GetAsset("gg.PNG").Data,
+                "bb": images.GetAsset("bb.PNG").Data,
+                "yy": images.GetAsset("yy.PNG").Data,        
+                "rn": images.GetAsset("rn.PNG").Data,
+                "gn": images.GetAsset("gn.PNG").Data,
+                "bn": images.GetAsset("bn.PNG").Data,
+                "yn": images.GetAsset("yn.PNG").Data,
+                "rg": images.GetAsset("rg.PNG").Data,
+                "rb": images.GetAsset("rb.PNG").Data,
+                "ry": images.GetAsset("ry.PNG").Data,
+                "gr": images.GetAsset("gr.PNG").Data,
+                "gb": images.GetAsset("gb.PNG").Data,
+                "gy": images.GetAsset("gy.PNG").Data,
+                "br": images.GetAsset("br.PNG").Data,
+                "by": images.GetAsset("by.PNG").Data,
+                "bg": images.GetAsset("bg.PNG").Data,
+                "yr": images.GetAsset("yr.PNG").Data,
+                "yb": images.GetAsset("yb.PNG").Data,
+                "yg": images.GetAsset("yg.PNG").Data                
+        }
+        
 def Initialize(tc):
     try:
-        task = tc.StroopTask
-        display = tc.Devices.Display
-        task.initialize(tc)
-        display.Display(task.getBlank())
+        tc.Devices.Display.Display(getBlank(tc))
         return True
     except Exception as e:
         Log.Error("An exception {e}: {trace}".format(e = e, trace = traceback.format_exc()))
         return False    
 
 def Stimulate(tc, x):
-    retValue = True
-    
     try:
-        task = tc.StroopTask
-        display = tc.Devices.Display
+        tc.Devices.Display.Display(tc.Images[tc.StimulusName])       
+        return True
     
-        if (tc.StimulusName == "Congruent"):
-                display.Display(task.getCongruent(), tc.DisplayTime)
-        elif (tc.StimulusName == "Inconguent"):
-            display.Display(task.getIncongruent(), tc.DisplayTime)
-        elif (tc.StimulusName == "Neutral"):
-            display.Display(task.getNeutral(), tc.DisplayTime)
-        else:
-            raise ValueError("Unknown stimulus type")       
     except Exception as e:
         Log.Error("An exception {e}: {trace}".format(e = e, trace = traceback.format_exc()))
-        retValue = False
-        
-    return retValue
+        return False
