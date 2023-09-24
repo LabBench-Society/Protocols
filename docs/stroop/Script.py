b2d237c5496dcc94c2c23d8808b6df39025b155c
Kristian Hennings <krhe@inventors.dk>
2023-09-24 16:26:31 +0200

    Work on simplifying the Stroop task

--- docs/stroop/Script.py
+++ docs/stroop/Script.py
@@ -8,24 +8,13 @@ def getImages(tc):
     else:
         return tc.Assets.EnglishImages
     
-def getBlank(tc):
-    return getImages(tc).GetAsset("blank.PNG").Data
-        
 def Initialize(tc):
-    try:
-        tc.Devices.Display.Display(getBlank(tc))
-        return True
-    except Exception as e:
-        Log.Error("An exception {e}: {trace}".format(e = e, trace = traceback.format_exc()))
-        return False    
+    display = tc.Devices.Display
+    display.Display(getImages(tc).GetAsset("blank.PNG").Data)
+    return True
 
 def Stimulate(tc, x):
-    try:
-        images = getImages(tc)
-        key = "{name}.PNG".format(name = tc.StimulusName)
-        tc.Devices.Display.Display(images.GetAsset(key).Data)       
-        return True
-    
-    except Exception as e:
-        Log.Error("An exception {e}: {trace}".format(e = e, trace = traceback.format_exc()))
-        return False
+    key = "{name}.PNG".format(name = tc.StimulusName)   
+    display = tc.Devices.Display
+    display.Display(getImages(tc).GetAsset(key).Data)       
+    return True
