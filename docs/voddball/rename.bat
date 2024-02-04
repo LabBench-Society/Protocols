7c97bd58dadba455ea493c79e7fc66267988dd95
Kristian Hennings <krhe@inventors.dk>
2024-02-04 17:53:00 +0100

    Created stimuli for the visual oddball task

---
+++ docs/voddball/rename.bat
@@ -0,0 +1,12 @@
+cd Stimuli
+
+move Slide1.PNG blank.png
+move Slide2.PNG normal.png
+move Slide3.PNG oddball.png
+move Slide4.PNG countingTask.png
+move Slide5.PNG responseTask.png
+move Slide6.PNG cross.png
+
+cd ..
+
+pause
