21bcd3d729c735f9b916362c9f80acc4b9c1465e
Kristian Hennings <krhe@inventors.dk>
2024-09-16 07:55:02 +0200

    Added threshold estimation protocol

---
+++ thrEstimation/rename.bat
@@ -0,0 +1,13 @@
+cd Illustrations
+copy Slide1.PNG ..\CueAFC.png
+copy Slide2.PNG ..\ProbeAFC.png
+copy Slide3.PNG ..\Cue01.png
+copy Slide4.PNG ..\Cue02.png
+copy Slide5.PNG ..\Cue03.png
+copy Slide6.PNG ..\Cue04.png
+copy Slide7.PNG ..\ProbeIFC.png
+copy Slide8.PNG ..\InstructionAFC.png
+copy Slide9.PNG ..\InstructionIFC.png
+
+pause
+
