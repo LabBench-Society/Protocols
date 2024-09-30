9287737db9fcce426c69f1e680c9bc0d5c1b0f16
Kristian Hennings <krhe@inventors.dk>
2024-09-30 12:29:15 +0200

    Started refactoring the Stroop task

--- stroop/Script.py
+++ stroop/Script.py
@@ -1,27 +1,4 @@
-﻿from Serilog import Log
-import traceback
-import random
-
-def getImages(tc):
-    if tc.Language == "DA":
-        return tc.Assets.DanishImages
-    else:
-        return tc.Assets.EnglishImages
-
-def NeutralInstructions(tc):
-    display = tc.Devices.Display
-    display.Display(getImages(tc).GetAsset("ninstruct.png").Data)
-    return True
-    
-def Instructions(tc):
-    display = tc.Devices.Display
-    display.Display(getImages(tc).GetAsset("instruct.png").Data)
-    return True
-
-def Initialize(tc):
-    display = tc.Devices.Display
-    display.Display(getImages(tc).GetAsset("blank.png").Data)
-    return True
+﻿import random
 
 def Stimulate(tc, x):
     key = "{name}.png".format(name = tc.StimulusName)   
@@ -33,18 +10,18 @@ def IsCorrect(result):
     name = result.Stimulus
     
     if (name[0] == 'b'):
-        return 1 if result.Response == 1 else 0
+        return True if result.Response == 1 else False
     elif (name[0] == 'y'):
-        return 1 if result.Response == 2 else 0
+        return True if result.Response == 2 else False
     elif (name[0] == 'r'):
-        return 1 if result.Response == 3 else 0
+        return True if result.Response == 3 else False
     elif (name[0] == 'g'):
-        return 1 if result.Response == 4 else 0
+        return True if result.Response == 4 else False
     else:
         return 0
     
-def Evaluate(tc):
+def StroopEvaluate(tc):
     result = tc.Current
-    tc.Current.Annotations.Add("correct", [IsCorrect(s) for s in result.Stimulations])
+    tc.Current.Annotations.SetBools("correct", [IsCorrect(s) for s in result.Stimulations])
     
     return True
