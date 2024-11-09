2133ab092ce15c41e0a42d47e747d5c37e8cc10e
Kristian Hennings <krhe@inventors.dk>
2024-11-09 08:59:07 +0100

    Refactoring of cognitive tasks (#30)
    * Created stopsignalGame protocol for gamified version of the stop-signal task
    
    * Refactoring of the conventional stop-signal-task
    
    * First draft of a refactored conventional stop signal task
    
    * Conventional stop-signal tested and working
    
    * Removed unused slide from the StopSignalImages
    
    * Deleted unused font
    
    * Work on refactoring the stop signal game protocol
    
    * Updated code of the stop signal game
    
    * Code works now, still missing game instructions
    
    * Finished the stop signal game
    
    * Stop signal task updated to LabBench 5 format
    
    * Started to refactor the Stroop tasks
    
    * Now it loads correctly in Runner and display instructions
    
    * Update StroopScript.py
    
    * Stroop task refactored
    
    * Work on refactoring the reverse stroop task
    
    * Fixed the reverse stroop task

---
+++ stroopReverse/StroopReverseScript.py
@@ -0,0 +1,76 @@
+﻿import random
+
+class Position:
+    def __init__(self, display):
+        self.Size = display.Height/12
+        self.Y1 = display.Height/6
+        self.Y2 = display.Height/2
+        self.Y3 = display.Height - self.Y1
+
+        self.X1 = display.Width/2 - (self.Y2 - self.Y1)
+        self.X2 = display.Width/2
+        self.X3 = display.Width/2 + (self.Y2 - self.Y1)
+
+def DrawResponses(tc, position, canvas):
+    canvas.Color(tc.StroopColors['r'])
+    canvas.Circle(position.X2, position.Y1, position.Size)
+    canvas.Color(tc.StroopColors['g'])
+    canvas.Circle(position.X3, position.Y2, position.Size)
+    canvas.Color(tc.StroopColors['b'])
+    canvas.Circle(position.X2, position.Y3, position.Size)
+    canvas.Color(tc.StroopColors['y'])
+    canvas.Circle(position.X1, position.Y2, position.Size)
+
+def ReverseStroopNeutralStimulate(tc, x):
+    display = tc.Instruments.ImageDisplay
+    name =  tc.StimulusName
+    position = Position(display)
+
+    with tc.Image.GetCanvas(display) as canvas:
+        canvas.Fill(True)
+        DrawResponses(tc, position, canvas)
+
+        canvas.Color(tc.StroopColors[name[0]])
+        canvas.Rectangle(position.X2 - position.Size, position.Y2 - position.Size,position.X2 + position.Size, position.Y2 + position.Size)
+
+        display.Display(canvas, tc.StroopDisplayTime, tc.ExperimentalSetup == 'LIO')
+        
+    return True
+
+def ReverseStroopStimulate(tc, x):
+    display = tc.Instruments.ImageDisplay
+    name =  tc.StimulusName
+    position = Position(display)
+
+    with tc.Image.GetCanvas(display) as canvas:
+        canvas.Fill(True)
+        DrawResponses(tc, position, canvas)
+        canvas.AlignCenter()
+        canvas.AlignMiddle()
+        canvas.Font("Roboto")
+        canvas.TextSize(100)
+
+        canvas.Color(tc.StroopColors[name[1]])
+        canvas.Write(display.Width/2, display.Height/2, tc.StroopWords[name[0]])
+
+        display.Display(canvas, tc.StroopDisplayTime, tc.ExperimentalSetup == 'LIO')
+
+    return True
+
+
+def IsCorrect(tc, result):
+    if (result.Stimulus[0] == 'b'):
+        return True if result.Response == 1 else False
+    elif (result.Stimulus[0] == 'y'):
+        return True if result.Response == 2 else False
+    elif (result.Stimulus[0] == 'r'):
+        return True if result.Response == 3 else False
+    elif (result.Stimulus[0] == 'g'):
+        return True if result.Response == 4 else False
+    else:
+        tc.Log.Error("Invalid stimulus name: " + result.Stimulus)
+        return False
+    
+def StroopEvaluate(tc):
+    tc.Current.Annotations.SetBools("correct", [IsCorrect(tc, s) for s in tc.Current.Stimulations])   
+    return True
