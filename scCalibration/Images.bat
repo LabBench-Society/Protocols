2a3b1db5ffe122ce8f5f833f950baf6e8a7a63df
Kristian Hennings <krhe@inventors.dk>
2024-11-11 10:45:10 +0100

    Work on instructions

--- scCalibration/Images.bat
+++ scCalibration/Images.bat
@@ -1,10 +1,16 @@
 cd Images
-move Slide1.PNG FiducialInstruction.png
-move Slide2.PNG DetectorInstruction.png
-move Slide3.PNG S1.png
-move Slide4.PNG S2.png
-move Slide5.PNG S3.png
-move Slide6.PNG S4.png
+move Slide1.PNG ScreenSizeInstruction.png
+move Slide2.PNG ScreenPositionInstruction.png
+move Slide3.PNG PositionInstruction.png
+move Slide4.PNG Blank.png
+move Slide5.PNG UpdateInstruction.png
+move Slide6.PNG FiducialInstruction.png
+move Slide7.PNG FiducialCheck.png
+move Slide8.PNG DetectorInstruction.png
+move Slide9.PNG S1.png
+move Slide10.PNG S2.png
+move Slide11.PNG S3.png
+move Slide12.PNG S4.png
 cd ..
 
 pause
