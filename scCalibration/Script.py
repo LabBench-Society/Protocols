6944fb97c7fd93f6ed9cbc0ac45daa50fe30221e
Kristian Hennings <krhe@inventors.dk>
2024-11-11 11:34:34 +0100

    Added the required tests + export of calibration record

--- scCalibration/Script.py
+++ scCalibration/Script.py
@@ -1,5 +1,22 @@
+
+def StartSize(tc):
+   tc.Instruments.ImageDisplay.Display(tc.Assets.Images.ScreenSizeInstruction, False)
+   return True
+
+def StartPosition(tc):
+   tc.Instruments.ImageDisplay.Display(tc.Assets.Images.ScreenPositionInstruction, False)
+   return True
+
 def StartFiducial(tc):
-   tc.Instruments.ImageDisplay.Display(tc.Assets.Images.FiducialInstruction, True)
+   tc.Instruments.ImageDisplay.Display(tc.Assets.Images.Blank, False)
+   return True
+
+def StartUpdate(tc):
+   tc.Instruments.ImageDisplay.Display(tc.Assets.Images.UpdateInstruction, True)
+   return True
+
+def StartVerify(tc):
+   tc.Instruments.ImageDisplay.Display(tc.Assets.Images.FiducialCheck, True)
    return True
 
 def CheckTriggers(tc):
@@ -9,5 +26,5 @@ def Initialize(tc):
    return True
 
 def Stimulate(tc,x):
-   tc.Instruments.ImageDisplay.Display(tc.Assets.Images[tc.StimulusName], True)
+   tc.Instruments.ImageDisplay.Display(tc.Assets.Images[tc.StimulusName], 500, True)
    return True
