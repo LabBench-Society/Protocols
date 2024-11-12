04f8b9a36f35cf71a88ac5a5205c1fd61a7da264
Kristian Hennings <krhe@inventors.dk>
2024-11-12 08:24:05 +0100

    Finished the Fiducial Calibration Protocol

--- scCalibration/Script.py
+++ scCalibration/Script.py
@@ -12,7 +12,7 @@ def StartFiducial(tc):
    return True
 
 def StartUpdate(tc):
-   tc.Instruments.ImageDisplay.Display(tc.Assets.Images.UpdateInstruction, True)
+   tc.Instruments.ImageDisplay.Display(tc.Assets.Images.UpdateInstruction, False)
    return True
 
 def StartVerify(tc):
@@ -20,11 +20,22 @@ def StartVerify(tc):
    return True
 
 def CheckTriggers(tc):
-   return True
+   actualTriggers = tc.Instruments.TriggerDetector.Count
+   expectedTriggers = 4 * tc.NumberOfRepetitions
+
+   if actualTriggers == expectedTriggers:
+      tc.Log.Information("Test PASSED! Expected {expectedTriggers} triggers and received {actualTriggers} triggers.", expectedTriggers, actualTriggers)
+   else:
+      tc.Log.Error("Test FAILED! Expected {expectedTriggers} triggers and received {actualTriggers} triggers.", expectedTriggers, actualTriggers)
+
+   return actualTriggers == expectedTriggers
 
 def Initialize(tc):
+   tc.Instruments.TriggerDetector.Reset()
    return True
 
 def Stimulate(tc,x):
    tc.Instruments.ImageDisplay.Display(tc.Assets.Images[tc.StimulusName], 500, True)
+   triggers = tc.Instruments.TriggerDetector.Count
+   tc.Log.Information("Number of detected triggers: {triggers}", triggers)
    return True
