5d748140d8e7243b215249abfe0654bff03c9ab1
Kristian Hennings <krhe@inventors.dk>
2024-11-12 08:46:20 +0100

    Calibration Protocol for Screens and Fiducials (#32)
    * Work on the fiducial calibration protocol
    
    * Update Images.pptx
    
    * Work on instructions
    
    * Work on instructions
    
    * Added the required tests + export of calibration record
    
    * Added operator instructions
    
    * Updated instructions
    
    * Finished the Fiducial Calibration Protocol
    
    * Updated instructions
    
    * Update Images.pptx

---
+++ scCalibration/Images.bat
@@ -0,0 +1,16 @@
+cd Images
+move Slide1.PNG  ScreenSizeInstruction.png
+move Slide2.PNG  ScreenPositionInstruction.png
+move Slide3.PNG  PositionInstruction.png
+move Slide4.PNG  Blank.png
+move Slide5.PNG  UpdateInstruction.png
+move Slide6.PNG  VerifyInstruction.png
+move Slide7.PNG  FiducialCheck.png
+move Slide8.PNG  TestInstruction.png
+move Slide9.PNG  S1.png
+move Slide10.PNG S2.png
+move Slide11.PNG S3.png
+move Slide12.PNG S4.png
+cd ..
+
+pause
