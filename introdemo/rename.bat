0f323c64e0c3f356d4978286f57b92a289fcea83
Kristian Hennings <krhe@inventors.dk>
2024-12-15 12:04:25 +0100

    Updated the introduction to LabBench protocol

--- introdemo/rename.bat
+++ introdemo/rename.bat
@@ -1,17 +1,9 @@
-cd Images_DA
+cd DassImages_DA
 move Slide1.PNG DASS.png
-move Slide2.PNG SR01.png
-move Slide3.PNG SR02.png
-move Slide4.PNG TS.png
-move Slide5.PNG CPM.png
 cd ..
 
-cd Images_EN
+cd DassImages_EN
 move Slide1.PNG DASS.png
-move Slide2.PNG SR01.png
-move Slide3.PNG SR02.png
-move Slide4.PNG TS.png
-move Slide5.PNG CPM.png
 cd ..
 
 pause
