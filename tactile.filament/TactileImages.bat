5f063aa0c432667f2ec10d84eae5259468336a36
Kristian Hennings <krhe@inventors.dk>
2025-05-23 10:26:20 +0200

    Added tactile protocols

---
+++ tactile.filament/TactileImages.bat
@@ -0,0 +1,12 @@
+cd TactileImages
+move Slide1.PNG Filament.png
+move Slide2.PNG NoFilament.png
+move Slide3.PNG TwoProngs.png
+move Slide4.PNG OneProng.png
+move Slide5.PNG TwoProngsAcross.png
+move Slide6.PNG TwoProngsAlong.png
+move Slide7.PNG GratingAcross.png
+move Slide8.PNG GratingAlong.png
+
+cd ..
+pause
