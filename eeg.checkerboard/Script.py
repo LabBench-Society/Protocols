26d8ad95004a6a84e99417035db69767bcf90ea3
Kristian Hennings <krhe@inventors.dk>
2025-07-20 16:49:33 +0200

    Work on the checkerboard EEG protocol

--- eeg.checkerboard/Script.py
+++ eeg.checkerboard/Script.py
@@ -6,16 +6,27 @@ class Checkerboard:
         self.F = 0.5
         self.A = 23.824;
 
-    def Initialize(self, N, F):
+    def Setup(self, N, F):
         display = self.tc.Instruments.ImageDisplay
         self.N = N
         self.F = F
+        self.Period = 1000 / F
         self.count = 0
         self.evenImage = self.CreateImage(0)
         self.oddImage = self.CreateImage(1)
         display.Display(self.evenImage)
 
         return True
+    
+    def GetPeriod(self):
+        return 1/self.F
+    
+    def Initialize(self):
+        display = self.tc.Instruments.ImageDisplay
+        display.Display(self.evenImage)
+        self.count = 0
+
+        return True      
     
     def CreateImage(self, remainder):
         display = self.tc.Instruments.ImageDisplay
@@ -24,22 +35,34 @@ class Checkerboard:
             metrics = display.Metrics
             L = metrics.AngleToPixels(self.A / self.N)
             canvas.Fill(True)
+            xoffset = (canvas.Width - self.N*L)/2
+            yoffset = (canvas.Height - self.N*L)/2
 
             for n in range(self.N):
                 for m in range(self.N):
-                    canvas.Color("#000000" if n*m % 2 == remainder else "#FFFFFF")
-                    canvas.Rectangle(m*L, n*L, (m+1)*L - 1, (n+1)* L)                    
+                    x1 = m*L + xoffset
+                    y1 = n*L + yoffset
+                    x2 = (m + 1) * L - 1 + xoffset
+                    y2 = (n + 1) * L - 1 + yoffset
+                    color = "#000000" if (m + (n % 2)) % 2 == remainder else "#FFFFFF"
+                    canvas.Color(color)
+                    canvas.Rectangle(x1, y1, x2, y2)                    
+
+            canvas.Color("#FF0000")
+            canvas.StrokeWidth(3)
+            canvas.Line(canvas.Width/2 - L, canvas.Height/2, canvas.Width/2 + L, canvas.Height/2)
+            canvas.Line(canvas.Width/2, canvas.Height/2 - L, canvas.Width/2, canvas.Height/2 + L)
 
             return canvas.GetImage() 
-
+        
     def Stimulate(self):
         display = self.tc.Instruments.ImageDisplay
         self.count = self.count + 1
+        image = self.evenImage if self.count % 2 == 0 else self.oddImage
 
-        if self.count % 2 == 0:
-            pass
-        else:
-            pass
+        self.tc.Scheduler.Run(self.tc.Scheduler.Create()
+                              .Add(self.Period/2, lambda: display.Display(image, True))
+                              .Add(self.Period/2, lambda: display.Display(image, False)))
 
         return True      
 
