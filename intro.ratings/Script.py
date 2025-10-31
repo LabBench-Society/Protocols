898143f01577aba3ef21b57f97b43acda6667899
Kristian Hennings <krhe@inventors.dk>
2025-10-31 15:51:52 +0100

    Work on CoScale demonstration protocol

--- intro.ratings/Script.py
+++ intro.ratings/Script.py
@@ -1,2 +1,17 @@
-def Sample(tc):
-   return [tc.Instruments.RatioScale.GetRatioRating()]
+import random
+
+class RandomWalk:
+   def __init__(self, tc):
+      self.tc = tc
+      self.value = 0
+      self.step = 0.2
+
+   def Sample(self):
+      scale = self.tc.Instruments.RatioScale
+      self.value = self.value - self.step if scale.Length * random.random() < scale.GetRatioRating()   else self.value + self.step
+      self.value = max(0, min(scale.Length, self.value))
+
+      return [self.value]
+
+def CreateRandomWalk(tc):  
+   return RandomWalk(tc)
