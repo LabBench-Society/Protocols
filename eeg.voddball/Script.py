fd9fdc77642e6e9715ae0d270f04d4e330c23482
Kristian Hennings <krhe@inventors.dk>
2026-02-11 14:39:14 +0100

    bugfixes

--- eeg.voddball/Script.py
+++ eeg.voddball/Script.py
@@ -1,4 +1,4 @@
 
 def Stimulate(tc, x):
-    tc.Instruments.ImageDisplay.Display(tc.Assets.Images.GetAsset(tc.StimulusName).Data, tc.DisplayTime, True)    
+    tc.Instruments.ImageDisplay.Display(tc.Assets.Images[tc.StimulusName], tc.DisplayTime, True)    
     return True
