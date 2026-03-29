a0a77986983c53f01ee3dce4e22fdd738b616837
Kristian Hennings <krhe@inventors.dk>
2026-03-29 18:32:10 +0200

    Added README conversion for the sequential demo protocol

--- intro.sequential/create_readme.bat
+++ intro.sequential/create_readme.bat
@@ -1,5 +1,5 @@
 REM pandoc README.md -o intro.labbench.docx --reference-doc=template.docx
 
-pandoc README.md -o intro.labbench.html --css=style.css -s --columns=999
+pandoc README.md -o intro.sequential.html --css=style.css -s --columns=999
 
 pause
