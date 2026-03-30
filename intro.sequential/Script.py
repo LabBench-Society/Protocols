19915aa98bba12f08ca57e7f60b6af5b310609da
Kristian Hennings <krhe@inventors.dk>
2026-03-30 11:26:41 +0200

    Working skeleton for the introduction to sequential procedures protocol

--- intro.sequential/Script.py
+++ intro.sequential/Script.py
@@ -23,7 +23,7 @@ class ResponseTask:
          image.AlignCenter()
          image.AlignMiddle()
          image.Color("#000000")
-         image.Write(x /2, y /2, "Rating: {r}".format(r = self.current))
+         image.Write(x /2, y /2, f"Rating: {self.current}")
          return image.GetImage()
       
    def Stimulate(self, intensity):    
@@ -43,118 +43,73 @@ class ResponseTask:
    def Enter(self, srTest):
       id = self.tc.CurrentState.ID
       display = self.tc.Instruments.ImageDisplay
-      self.tc.Keyboard.Clear();
+      self.tc.Instruments.Button.Reset();
 
       if id == "CROSS":
          display.Display(self.Cross)
-         return True
       if id == "SELECTION":
          display.Display(self.GenerateCues())
-         return True
       if id == "DISPLAY":
          display.Display(self.GenerateSelectedCue())
-         return True
       if id == "STIMULATION":
          self.Stimulate(50)
          display.Display(self.Stimulating)
-         return True
       if id == "RATING":
          self.tc.CurrentState.SetPlotter(lambda x, y: self.PlotRating(x,y))
-         return True
       if id == "RESET":
-         self.tc.CurrentState.SetPlotter(lambda x, y: self.PlotRating(x,y))
          display.Display(self.RatingInstruction)
-         return True
       if id == "PAUSE":
          self.current = 0
          self.tc.Log.Information("Actions; ESC) Abort, INSERT) Complete, ENTER) Continue.")
-         self.tc.CurrentState.SetPlotter(lambda x, y: self.PlotRating(x,y))
-         return True
       
-      return False
-   
-   
+      return True
+      
    def Leave(self):
-      id = self.tc.CurrentState.ID
-      self.tc.Keyboard.Clear();
-
-      if id == "CROSS":
-         return True
-      if id == "SELECTION":
-         return True
-      if id == "DISPLAY":
-         return True
-      if id == "STIMULATION":
-         return True
-      if id == "RATING":
-         return True
-      if id == "RESET":
-         return True
-      if id == "PAUSE":
-         return True
-      
       return True      
    
    def Update(self):
       id = self.tc.CurrentState.ID
-      self.tc.CurrentState.Status = "Running time: {time}".format(time = self.tc.CurrentState.RunningTime)
+      self.tc.CurrentState.Status = f"Running time: {self.tc.CurrentState.RunningTime}"
 
       if id == "CROSS":
-         return "*" if self.tc.CurrentState.RunningTime < 500 else "SELECTION"
+         return "*" if self.tc.CurrentState.RunningTime < 1000 else "SELECTION"      
       
       if id == "SELECTION":
-         if self.tc.CurrentState.RunningTime > 2000: 
+         if self.tc.CurrentState.RunningTime > 4000: 
             self.tc.Log.Information("No response, selecting one random selection")
             # Select a the lure
-            return "DISPLAY"
-         
+            return "DISPLAY"         
          if self.tc.Instruments.Button.IsLatched("1"):
             # Select the cue assigned to button 1
             return "DISPLAY"
-
          if self.tc.Instruments.Button.IsLatched("2"):
             # Select the cue assigned to button 2
-            return "DISPLAY"
-
-         return "*" 
-      
+            return "DISPLAY"      
+         
       if id == "DISPLAY":
-         return "*" if self.tc.CurrentState.RunningTime < 1000 else "STIMULATION"
-      
+         return "*" if self.tc.CurrentState.RunningTime < 2000 else "STIMULATION"  
+          
       if id == "STIMULATION":
-         return "*" if self.tc.CurrentState.RunningTime < 1000 else "RATING"
+         return "*" if self.tc.CurrentState.RunningTime < 2000 else "RATING"      
       
       if id == "RATING":
          button = self.tc.Instruments.Button
-         self.current = self.tc.Instruments.Scale.GetCurrentRating()
+         self.current = self.tc.Instruments.RatioScale.GetCurrentRating()
          self.tc.CurrentState.Changed = True
 
          if button.IsLatched("1") or button.IsLatched("2"):
-            return "RESET"
+            return "RESET"      
          
-         return "*" 
-      
       if id == "RESET":
-         if (self.tc.Instruments.Scale.GetCurrentRating() < 0.1):
-            return "PAUSE"
+         rating = self.tc.Instruments.RatioScale.GetCurrentRating()
+         self.tc.CurrentState.Status = f"Current rating: {rating}"
          
-         return "*"
-      
+         return "PAUSE" if rating < 0.5 else "*"
+             
       if id == "PAUSE":
-         if self.tc.Keyboard.Pressed("ESC"):
-            return "abort"
-         
-         if self.tc.Keyboard.Pressed("INSERT"):
-            self.ratings.append(self.current)
-            return "complete"
-         
-         if self.tc.Keyboard.Pressed("ENTER"):
-            self.ratings.append(self.current)
-            return "CROSS"
+         return "*" if self.tc.CurrentState.RunningTime < 1000 else "CROSS"      
 
-         return "*"
-
-      return "abort"
+      return "*"
 
 def CreateTask(tc):
    return ResponseTask(tc)
