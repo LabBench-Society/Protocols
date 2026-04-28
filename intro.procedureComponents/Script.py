3bbc0fe36df28c623c58e99c7b0988cae1b28098
Kristian Hennings <krhe@inventors.dk>
2026-04-28 09:14:44 +0200

    Added missing attributes

--- intro.procedureComponents/Script.py
+++ intro.procedureComponents/Script.py
@@ -1,4 +1,7 @@
 
+def Initialize(contect):
+    return True
+
 def Stimulate(context, x):
     context.Instruments.Stimulator.Generate("port2", context.Stimulus)
     context.Instruments.ImageDisplay.Display(context.Assets.Images.Cue, 250, True)
